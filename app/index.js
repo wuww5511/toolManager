8015cbf8266409753bd6df8e7be635efcf01ea01
wuweiwei <wuww5511@126.com>
2017-02-06 19:33:12 +0800

    关闭时验证命令是否结束

--- app/index.js
+++ app/index.js
@@ -113,37 +113,33 @@ function createWindow() {
   }
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
+  mainWindow.on('close', (event) => {
     
-    server.removeClient(clientId);
-  })
+    if(Object.keys(commands).length > 0) {
+        event.preventDefault();
+        dialog.showMessageBox({
+            message: "还有程序未结束，确定要退出程序吗？",
+            buttons: ["确定", "取消"]
+        }, (res) => {
+            if(res === 0) {
+                server.removeClient(clientId);
+                for(var i in commands) {
+                    commands[i].kill();
+                }
+                commands = {};
+                
+                mainWindow.close();
+            }
+        });
+    }
+  });
+    
+    mainWindow.on('closed', () => {
+        app.quit();
+    })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
